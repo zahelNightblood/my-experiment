9467fc03266158d93554e9443c6a6bc732d07da2
Matthais Pali <matthaispali@gmail.com>
2026-06-20 14:16:44 -0600

    first readd of niri

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,3 +12,7 @@ cp -avf "/ctx/system_files"/. /
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
+dnf5 install -y niri \
+	fuzzel \
+	pavucontrol \
+	sddm
